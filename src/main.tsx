f87022be09a4629d11ebc26405ac69113b4e5711
matheus.soares2 <matheus.soares@finderup.com.br>
2023-04-05 16:43:06 -0300

    feat(changes): add credit card

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import "./main.css";
 
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
@@ -8,8 +7,4 @@ import App from "./App";
 const container = document.getElementById("root");
 const root = createRoot(container as HTMLDivElement);
 
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+root.render(<App />);
