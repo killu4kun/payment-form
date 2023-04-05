31b9309c557096c5ab113659c9320e54f697c8f0
matheus.soares2 <matheus.soares@finderup.com.br>
2023-04-05 19:26:52 -0300

    feat(fix): add assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
 import CreditCard from "./components/CreditCard";
+import CreditCardOk from "./components/CreditCardConfirm";
+import CreditCardForm from "./components/CreditCardForm";
+
+import "./App.css";
 
 export default function App() {
+  const [validate] = useState(false);
+
   return (
     <>
       <CreditCard />
       <main className="cardOverflow">
-        <div></div>
+        <div>{validate ? <CreditCardOk /> : <CreditCardForm />}</div>
       </main>
       ;
     </>
---
+++ src/components/CreditCardConfirm/index.tsx
@@ -0,0 +1,20 @@
+import iconComplete from "../../assets/icon-complete.svg";
+
+function CreditCardOk() {
+  const resetForm = () => {
+    console.log("hello");
+  };
+
+  return (
+    <div className="cardThanks">
+      <img src={iconComplete} alt="" />
+      <p>Thank you!</p>
+      <p>We&apos;ve added your card details</p>
+      <button className="btn-primary" onClick={resetForm}>
+        Continue
+      </button>
+    </div>
+  );
+}
+
+export default CreditCardOk;
