b0e3216c55df798007c5a90816ffbc60ddb6f6b0
matheus.soares2 <matheus.soares@finderup.com.br>
2023-04-05 19:07:29 -0300

    feat(form): add form class

--- src/components/CreditCard/index.tsx
+++ src/components/CreditCard/index.tsx
@@ -1,18 +1,18 @@
-function CreditCard({ formData }) {
+import "./style.css";
+
+function CreditCard() {
   return (
     <aside className="cardDeco">
       <div className="cardFront">
-        <span>{formData.number || "0000 0000 0000 0000"}</span>
+        <span>{"0000 0000 0000 0000"}</span>
         <div>
-          <span>{formData.name || "Jane Appleseed"}</span>
-          <span>
-            {formData.mm || "00"}/{formData.yy || "00"}
-          </span>
+          <span> {"Jane Appleseed"}</span>
+          <span>00/ 00</span>
         </div>
       </div>
 
       <div className="cardBack">
-        <span>{formData.cvc || "000"}</span>
+        <span> 000</span>
       </div>
     </aside>
   );
