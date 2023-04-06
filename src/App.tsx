55e6528dd616d279d69931d80729f7cacccf4cf8
matheus.soares2 <matheus.soares@finderup.com.br>
2023-04-05 23:04:52 -0300

    feat : cleaning up stuff and improving code sintax

--- src/App.tsx
+++ src/App.tsx
@@ -32,8 +32,6 @@ export default function App() {
     }, 500);
   };
 
-  console.log(validate);
-
   return (
     <>
       <CreditCardContextProvider>
--- src/components/CreditCardForm/index.tsx
+++ src/components/CreditCardForm/index.tsx
@@ -12,53 +12,6 @@ export type FormValues = {
   cvc: string;
 };
 
-const resolver: Resolver<FormValues> = async values => {
-  const errors: FieldErrors<FormValues> = {};
-  if (!values.cardHolderName) {
-    errors.cardHolderName = {
-      type: "required",
-      message: "Card holder name is required",
-    };
-  }
-
-  if (!values.cardNumber) {
-    errors.cardNumber = {
-      type: "required",
-      message: "Card number is required",
-    };
-  }
-
-  if (!values.expirationDateYY || !values.expirationDateMM) {
-    errors.expirationDateYY = {
-      type: "required",
-      message: "Can't be blank",
-    };
-    errors.expirationDateMM = {
-      type: "required",
-      message: "Can't be blank",
-    };
-  }
-
-  if (!values.cvc) {
-    errors.cvc = {
-      type: "required",
-      message: "CVC is required",
-    };
-  }
-
-  if (Object.keys(errors).length > 0) {
-    return {
-      values,
-      errors,
-    };
-  }
-
-  return {
-    values,
-    errors: {},
-  };
-};
-
 function CreditCardForm({ animateSlider }: any) {
   const { formValues, setFormValues } = useFormData();
 
@@ -70,25 +23,9 @@ function CreditCardForm({ animateSlider }: any) {
     });
   };
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    reset,
-  } = useForm<FormValues>({ resolver });
-
-  const onSubmit = handleSubmit(data => {
-    reset();
+  const onSubmit = (event: any): void => {
+    event.preventDefault();
     animateSlider(true);
-  });
-
-  const removeErrorMessage = (event: React.FormEvent<HTMLInputElement>) => {
-    const target = event.target as HTMLInputElement;
-    const errorMessage = target.parentNode?.querySelector("span");
-
-    if (errorMessage) {
-      errorMessage.remove();
-    }
   };
 
   return (
@@ -96,30 +33,24 @@ function CreditCardForm({ animateSlider }: any) {
       <label className="labelname">
         Cardholder Name
         <input
-          {...register("cardHolderName")}
           onChange={handleInputChange}
           type="text"
           placeholder="e.g. Jane Appleseed"
           name="cardHolderName"
           className="card-input"
-          onBlur={removeErrorMessage}
         />
-        {errors?.cardHolderName && <span>{errors.cardHolderName.message}</span>}
       </label>
       <p className="info info--hidden" aria-live="polite"></p>
       <label className="labelnumber">
         Card Number
         <input
-          {...register("cardNumber")}
           onChange={handleInputChange}
           type="text"
           placeholder="e.g. 1234 5678 9123 0000"
           name="cardNumber"
           className="card-input"
-          onBlur={removeErrorMessage}
           minLength={19}
         />
-        {errors.cardNumber && <span>{errors.cardNumber.message}</span>}
       </label>
       <p className="info info--hidden" aria-live="polite"></p>
 
@@ -128,9 +59,7 @@ function CreditCardForm({ animateSlider }: any) {
           Exp. Date (MM/YY)
           <div>
             <input
-              {...register("expirationDateMM")}
               onChange={handleInputChange}
-              onBlur={removeErrorMessage}
               type="text"
               placeholder="MM"
               maxLength={2}
@@ -138,19 +67,14 @@ function CreditCardForm({ animateSlider }: any) {
               className="card-input"
             />
             <input
-              {...register("expirationDateYY")}
               onChange={handleInputChange}
               maxLength={2}
-              onBlur={removeErrorMessage}
               type="text"
               placeholder="YY"
               name="expirationDateYY"
               className="card-input"
             />
           </div>
-          {(errors.expirationDateYY || errors.expirationDateMM) && (
-            <span>{errors.expirationDateYY?.message}</span>
-          )}
         </label>
 
         <p className="info info--hidden" aria-live="polite"></p>
@@ -158,16 +82,13 @@ function CreditCardForm({ animateSlider }: any) {
         <label className="labelcvc">
           CVC
           <input
-            {...register("cvc")}
             onChange={handleInputChange}
             type="text"
             placeholder="e.g. 123"
             maxLength={3}
             name="cvc"
             className="card-input"
-            onBlur={removeErrorMessage}
           />
-          {errors.cvc && <span>{errors.cvc.message}</span>}
         </label>
         <p className="info info--hidden" aria-live="polite"></p>
       </div>
